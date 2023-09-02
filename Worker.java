237a37fdd0561961e6c1499c5da94ea91d588bb0
Alexey <cappuccino13371@gmail.com>
2023-09-02 18:12:54 +0300

    Add task 2 for lesson 3

---
+++ Worker.java
@@ -0,0 +1,16 @@
+public class Worker extends Employee{
+    public Worker(String name, String surName, double salary) {
+        super(name, surName, salary);
+    }
+
+    @Override
+    public double calculateSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s; Рабочий; Среднемесячная заработная плата (фиксированная) %.2f",
+                surName, name, salary);
+    }
+}
